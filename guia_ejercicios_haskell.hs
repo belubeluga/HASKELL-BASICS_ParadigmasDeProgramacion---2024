5207794246e66efb8122b292eae4fcf4a804e83f
Belubeluga <belugotz@gmail.com>
2024-02-29 20:54:34 -0300

    29/2

--- guia_ejercicios_haskell.hs
+++ guia_ejercicios_haskell.hs
@@ -33,4 +33,26 @@ import Data.Char
 -- (!!) :: GHC.Stack.Types.HasCallStack => [a] -> Int -> a      (devuelve el elemento en el indice indicado)
 -- elem :: (Foldable t, Eq a) => a -> t a -> Bool (chequea si un elemento esta en el foldable) //// elem 3 [1,2,4,3] => True //// elem 3 [1,2,4] => False
 
--- EJERCICIO 2
+{-  
+        || OR       && AND
+        mod RESTO   div PARTE ENTERA
+        == 
+
+-}
+
+
+{- EJERCICIO 2 -}
+import Data.Maybe
+import Data.Char
+import GHC.Real
+
+valorAbsoluto :: Float -> Float
+valorAbsoluto x |x < 0 = -x --valores negativos entre parentesis
+                 |otherwise = x
+
+-- divisble x 4 o 400 pero no x 100
+
+
+bisiesto :: Int -> Bool
+bisiesto x | (mod x 4 == 0 || mod x 400 == 0) && (mod x 100 /= 0) = True
+           |otherwise = False
